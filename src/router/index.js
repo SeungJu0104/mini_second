64cbbd5aee2999b9dba4f09b4b49337acee3c258
DESKTOP-684DG1A\SeungJu <skatmdwn23@naver.com>
2025-06-02 16:56:28 +0900

    ,

--- src/router/index.js
+++ src/router/index.js
@@ -13,16 +13,20 @@ import PostRegister from '@/components/board/PostRegister.vue'
 const router = createRouter({
     history : createWebHistory(),
     routes : [
+        // 게시판
         {path: '/', name: 'home', component: Home},
         {path: '/board/:category', name: 'postList', component: PostList},
         {path: '/board/detail/:postNo', name: 'postDetail', component: PostDetail },
         {path: '/board/update/:postNo', name: 'postUpdate', component: PostUpdate },
         {path: '/board/register/:board', name: 'postRegister', component: PostRegister },
+
+        // 회원
         {path: '/member/login', name: 'login', component: Login},
         {path: '/member/registerForm', name: 'memberRegisterForm', component: MemberRegisterForm},
         {path: '/member/updateForm', name: 'memberUpdateForm', component: MemberUpdateForm},
         {path: '/member/memberList', name: 'memberList', component: MemberList},
         {path: '/member/memberDetail', name : 'memberDetail', component : MemberDetail}
+
     ]
 })
 
